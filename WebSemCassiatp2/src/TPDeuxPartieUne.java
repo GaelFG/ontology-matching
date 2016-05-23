a583c353d60a2c5062743efa058395d20fa3d1e6
GaelFG <gael.fenet.garde@gmail.com>
2016-05-23 17:40:06 +0200

    CA marche

--- WebSemCassiatp2/src/TPDeuxPartieUne.java
+++ WebSemCassiatp2/src/TPDeuxPartieUne.java
@@ -1,31 +1,41 @@
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.Properties;
 
 import org.semanticweb.owl.align.Alignment;
+import org.semanticweb.owl.align.AlignmentException;
 import org.semanticweb.owl.align.AlignmentProcess;
 import org.semanticweb.owl.align.AlignmentVisitor;
 
 import fr.inrialpes.exmo.align.impl.method.ClassStructAlignment;
+import fr.inrialpes.exmo.align.impl.method.EditDistNameAlignment;
 import fr.inrialpes.exmo.align.impl.renderer.RDFRendererVisitor;
 
 public class TPDeuxPartieUne {
 
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		TPDeuxPartieUne prog = new TPDeuxPartieUne();
-		prog.questionUneGenerationDAlignements();
+		try {
+			prog.questionUneGenerationDAlignements();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void questionUneGenerationDAlignements() throws Exception {
 		URI UriFilmsToulouseOntologie = new URI("http://www.irit.fr/~Cassia.Trojahn/insa/FilmsToulouse.owl");
-		URI UriNotreOntologie = new URI("./maFilmographie.owl");//TODO METTRE VRAIE URL
+		//URI UriNotreOntologie = new URI("http://mappings.dbpedia.org/server/ontology/dbpedia.owl");
+		URI UriNotreOntologie = new URI("file:///C:/Users/gwend/git/tpcassia/WebSemCassiatp2/maFilmographie.owl");//TODO METTRE VRAIE URL
 		URI UriDBPediaOntologie = new URI("http://mappings.dbpedia.org/server/ontology/dbpedia.owl");
 		
 		// paire d'ontologie (votre ontologie et FilmsToulouse)
@@ -36,11 +46,12 @@ public class TPDeuxPartieUne {
 	}
 	
 	// On genere un alignement et on en check la qualité selon trois methides differentes
-	public void traiterUnePaireDOntologies(URI ontologieA, URI ontologieB, String prefixNomFichier) throws Exception{
+	public void traiterUnePaireDOntologies(URI ontologieA, URI ontologieB, String prefixNomFichier) throws FileNotFoundException, UnsupportedEncodingException, AlignmentException{
 		Alignment alignement;
 		//choisissez l'un des matcheurs implementé par l'API d'Alignement et que vous avez 
 		 //testé lors du premier TP ; 
-		alignement = genererAlignement(ontologieA, ontologieB, new ClassStructAlignment());
+		//TODO penser a preciser recherhe de la methode d'alignement apes plusieur essais blabla
+		alignement = genererAlignement(ontologieA, ontologieB, new EditDistNameAlignment());
 		render(alignement, "./alignement"+prefixNomFichier+"-de-base.rdf");
 		
 		// 2. développez le matcheur basé sur la comparaison de labels indiqué ci-dessous ;
@@ -60,11 +71,14 @@ public class TPDeuxPartieUne {
 	 * @param onto2 la deuxième ontologie
 	 * @param process La methode d'alignement a utiliser
 	 * @return Un alignement d'ontologies
+	 * @throws AlignmentException 
 	 * @throws Exception
 	 */
-	public static Alignment genererAlignement(URI onto1, URI onto2, AlignmentProcess process) throws Exception {
+	public static Alignment genererAlignement(URI onto1, URI onto2, AlignmentProcess process) throws AlignmentException {
+
 		process.init (onto1, onto2); 
 		process.align(null, new Properties());
+		System.out.println(process.nbCells());
 		return process;
 	}
 	
@@ -72,9 +86,12 @@ public class TPDeuxPartieUne {
 	 * Génére le fichier d'alignement rdf .
 	 * @param alignment L'alignement à exporter sous forme de fichier
 	 * @param cheminFichier Le chemin du fichier à générer
+	 * @throws AlignmentException 
+	 * @throws FileNotFoundException 
+	 * @throws UnsupportedEncodingException 
 	 * @throws Exception Kancamarchepa
 	 */
-	public static void render(Alignment alignment, String cheminFichier) throws Exception { 
+	public static void render(Alignment alignment, String cheminFichier) throws AlignmentException, FileNotFoundException, UnsupportedEncodingException { 
 		PrintWriter writer; 
 		FileOutputStream f = new FileOutputStream(new File(cheminFichier)); 
 		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(f,"UTF-8")), true); 
